9657f817eb97272837c1cefe8906518fd9b0e7eb
Aritya Vitendra Arjunan <aritya.wps@gmail.com>
2026-01-16 12:16:49 +1100

    Fix: Add verified parameter to Cask url

--- Casks/crawl-tiles.rb
+++ Casks/crawl-tiles.rb
@@ -1,9 +1,9 @@
 cask "crawl-tiles" do
   version "0.33.1"
-  # This is the SHA for the Cask:
   sha256 "aa63f58d606afdd158bc6b0139c97b0e36a4062cb73641e637d03e2732c032b5"
 
-  url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-tiles-universal.zip"
+  url "https://github.com/crawl/crawl/releases/download/#{version}/dcss-#{version}-macos-tiles-universal.zip",
+      verified: "github.com/crawl/crawl/"
   name "Dungeon Crawl Stone Soup"
   desc "Roguelike dungeon crawler with tiles graphics"
   homepage "https://crawl.develz.org/"
